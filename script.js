214077cd3588cceac2ad4e0daa35a0612e511bdf
Harsh Sharma <102505510+HNSharma-07@users.noreply.github.com>
2024-01-13 00:16:13 +0530

    up

---
+++ script.js
@@ -0,0 +1,23 @@
+var typeData = new Typed(".role", {
+  strings: ["Coder", "Web Developer", "Data Enthusiast", "Keen Learner"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});
+
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+
+// //   Modal open function
+// const openModal = () => {
+//   console.log("Modal is Open");
+//   modal.classList.add("active");
+//   overlay.classList.add("overlayactive");
+// };
+
+// // Modal close function
+// const closeModal = () => {
+//   modal.classList.remove("active");
+//   overlay.classList.remove("overlayactive");
+// };
